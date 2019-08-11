3af74d6b3cc76a2fe6988ce8ece46d0483224f50
Christian Ramelow <info@codekandis.net>
2019-08-11 17:36:03 +0200

    Adds the `MessagesFixture`.

---
+++ tests/Fixtures/MessagesFixture.php
@@ -0,0 +1,28 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\CodeMessageInterpreter\Tests\Fixtures;
+
+/**
+ * Represents an messages fixture.
+ * @package codekandis/code-message-interpreter
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class MessagesFixture
+{
+	/**
+	 * Represents the first code.
+	 * @var int
+	 */
+	public const FIRST_CODE = 'Represents the first code.';
+
+	/**
+	 * Represents the second code.
+	 * @var int
+	 */
+	public const SECOND_CODE = 'Represents the second code.';
+
+	/**
+	 * Represents the third code.
+	 * @var int
+	 */
+	public const THIRD_CODE = 'Represents the third code.';
+}
