095233b0bf98c514a61e68c84af53df1d06f0f09
Christian Ramelow <info@codekandis.net>
2019-08-11 17:35:29 +0200

    Adds the `CodeMessageInterpreterException`.

---
+++ src/CodeMessageInterpreterException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\CodeMessageInterpreter;
+
+use LogicException;
+
+/**
+ * Represents an exception if an error occurres during the code interpretation.
+ * @package codekandis/code-message-interpreter
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class CodeMessageInterpreterException extends LogicException
+{
+}
